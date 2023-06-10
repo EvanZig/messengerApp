13be747c29ea69f6ea6632f44e0fa613f8ffe87f
GitSkrud <euaggeloszigas@gmail.com>
2023-06-10 20:30:35 +0300

    added placeholder buttons for chat input

--- client/src/components/ChatUi/ChatInput.tsx
+++ client/src/components/ChatUi/ChatInput.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Input } from 'antd'
+import { Input, Button } from 'antd'
 import '../../styles/componentStyling/ChatLayoutStyle.scss'
 
 const { TextArea } = Input
 
 export default function ChatInput() {
     return (
-        <div>
+        <div style={{ display: 'flex' }}>
             <TextArea
                 placeholder="Send message"
                 autoSize={{ minRows: 1, maxRows: 4 }}
                 className="chatInputBox"
             ></TextArea>
+            <Button> emoji </Button>
+            <Button>Send</Button>
         </div>
     )
 }
