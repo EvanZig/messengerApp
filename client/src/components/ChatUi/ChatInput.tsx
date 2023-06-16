559eff694f5b00d0812ddf4ba60c0171ef977764
GitSkrud <euaggeloszigas@gmail.com>
2023-06-16 15:11:42 +0300

    added emoji icon

--- client/src/components/ChatUi/ChatInput.tsx
+++ client/src/components/ChatUi/ChatInput.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Input, Button } from 'antd'
 import '../../styles/componentStyling/ChatLayoutStyle.scss'
+import { SmileOutlined } from '@ant-design/icons'
 
 const { TextArea } = Input
+// make it circular only to the left so it looks like its one div the entire thing
 
 export default function ChatInput() {
     return (
         <div style={{ display: 'flex' }}>
             <TextArea
-                placeholder="Send message"
+                placeholder="Send message . . ."
                 autoSize={{ minRows: 1, maxRows: 4 }}
                 className="chatInputBox"
             ></TextArea>
-            <Button> emoji </Button>
-            <Button>Send</Button>
+            <div className="chatInputButtons" style={{ display: 'flex' }}>
+                <Button icon={<SmileOutlined />} className="emojiButton" />
+                <Button>Send</Button>
+            </div>
         </div>
     )
 }
