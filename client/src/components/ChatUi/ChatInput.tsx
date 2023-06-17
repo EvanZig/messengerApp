0017bafb899986e44403bf47595b94d27710fce0
GitSkrud <euaggeloszigas@gmail.com>
2023-06-17 07:19:37 +0300

    added emoji picker and functionality

--- client/src/components/ChatUi/ChatInput.tsx
+++ client/src/components/ChatUi/ChatInput.tsx
@@ -1,22 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Input, Button } from 'antd'
 import '../../styles/componentStyling/ChatLayoutStyle.scss'
 import { SmileOutlined } from '@ant-design/icons'
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data'
 
 const { TextArea } = Input
+
 // make it circular only to the left so it looks like its one div the entire thing
 
 export default function ChatInput() {
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+    const emojiPickerRef = useRef<HTMLDivElement>(null)
+    const [textareaValue, setTextareaValue] = useState('')
+
+    const handleEmojiButtonClick = () => {
+        setShowEmojiPicker(!showEmojiPicker)
+    }
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (
+                emojiPickerRef.current &&
+                !emojiPickerRef.current.contains(event.target as Node)
+            ) {
+                setShowEmojiPicker(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [])
+
     return (
-        <div style={{ display: 'flex' }}>
+        <div className="chatInputWrapper">
             <TextArea
+                value={textareaValue}
+                onChange={(e) => setTextareaValue(e.target.value)}
                 placeholder="Send message . . ."
                 autoSize={{ minRows: 1, maxRows: 4 }}
                 className="chatInputBox"
             ></TextArea>
-            <div className="chatInputButtons" style={{ display: 'flex' }}>
-                <Button icon={<SmileOutlined />} className="emojiButton" />
-                <Button>Send</Button>
+            <Button
+                icon={<SmileOutlined />}
+                className="emojiButton chatInputButton"
+                onClick={handleEmojiButtonClick}
+            />
+            <Button className="chatInputButton sendButton">Send</Button>
+            <div
+                className="emojiPicker"
+                style={
+                    showEmojiPicker ? { display: 'block' } : { display: 'none' }
+                }
+                ref={emojiPickerRef}
+            >
+                <Picker
+                    data={data}
+                    previewPosition="none"
+                    onEmojiSelect={(e: any) => {
+                        setShowEmojiPicker(false)
+                        const appendedText = textareaValue + e.native
+                        setTextareaValue(appendedText)
+                    }}
+                />
             </div>
         </div>
     )
